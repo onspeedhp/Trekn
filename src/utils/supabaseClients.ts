0c3ffd574f4525dde29a2b7ea7c72b41c4a12b46
onspeedhp <chaukhac4@gmail.com>
2023-09-24 06:03:23 +0700

    Update supabase url

--- src/utils/supabaseClients.ts
+++ src/utils/supabaseClients.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
