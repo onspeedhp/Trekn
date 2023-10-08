c5f695aa8536b073b6db3bdc4fe2c43ea784b535
onspeedhp <chaukhac4@gmail.com>
2023-10-08 15:37:43 +0700

    update

--- src/axios/index.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const request = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND,
-  headers: { 'Content-Type': 'application/json' },
-});
-
-export default request;
